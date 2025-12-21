5bc01b4934409072a7245b10affe160d43edf3ce
Sergey Eliseev <s.eliseev1@gmail.com>
2025-12-21 08:51:53 +0300

    2. HQL [#6874]

---
+++ db/scripts/003_dml_update.sql
@@ -0,0 +1,3 @@
+INSERT INTO auto_user (login, password) VALUES ('Ivanov', 'password123');
+INSERT INTO auto_user (login, password) VALUES ('Petrov', 'password123');
+INSERT INTO auto_user (login, password) VALUES ('Sidorov', 'password123');
---
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -0,0 +1,98 @@
+package ru.job4j.repository;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import ru.job4j.model.User;
+import java.util.List;
+import java.util.Optional;
+
+public class UserRepository {
+    private final SessionFactory sf;
+
+    public UserRepository(SessionFactory sf) {
+        this.sf = sf;
+    }
+
+    public User create(User user) {
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.save(user);
+                transaction.commit();
+                return user;
+            } catch (Exception e) {
+                transaction.rollback();
+                throw new RuntimeException("Failed to save user", e);
+            }
+        }
+    }
+
+    public void update(User user) {
+        try (Session session = sf.openSession()) {
+            var transaction = session.beginTransaction();
+            try {
+                session.update(user);
+                transaction.commit();
+            } catch (Exception e) {
+                transaction.rollback();
+                throw new RuntimeException("Failed to update user", e);
+            }
+        }
+    }
+
+    public void delete(Integer userId) {
+        try (Session session = sf.openSession()) {
+            var transaction = session.beginTransaction();
+            try {
+                User user = session.get(User.class, userId);
+                if (user != null) {
+                    session.delete(user);
+                }
+                session.delete(user);
+                transaction.commit();
+            } catch (Exception e) {
+                transaction.rollback();
+                throw new RuntimeException("Failed to delete user", e);
+            }
+        }
+    }
+
+    public List<User> findAllOrderById() {
+        try (Session session = sf.openSession()) {
+            return session.createQuery("FROM User ORDER BY id", User.class).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve from database", e);
+        }
+    }
+
+    public Optional<User> findById(Integer userId) {
+        try (Session session = sf.openSession()) {
+            return Optional.ofNullable(session.get(User.class, userId));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve from database", e);
+        }
+    }
+
+    public List<User> findByLikeLogin(String key) {
+        try (Session session = sf.openSession()) {
+            return session.createQuery("FROM User WHERE LOWER(login) LIKE LOWER(:key)", User.class)
+                    .setParameter("key", "%" + key + "%")
+                    .list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve from database", e);
+        }
+    }
+
+    public Optional<User> findByLogin(String login) {
+        try (Session session = sf.openSession()) {
+            return session.createQuery("FROM User WHERE login =:login", User.class)
+                    .setParameter("login", login)
+                    .uniqueResultOptional();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve from database", e);
+        }
+    }
+
+
+}
